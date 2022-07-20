7bd904a574414cbb7032986050d349034e2b352e
Erick Maese <maese99@gmail.com>
2022-07-20 15:15:19 -0700

    setup wabpack & babel

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import  ReactDOM  from "react-dom";
+import React from "react";
+
+const App = () => {
+    return(
+        <h1>this is ericks react app</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
